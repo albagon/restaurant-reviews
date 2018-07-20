5779be8b6b4323230afb2634f61e948c955fa8fb
albagonzalez <albupas@hotmail.com>
2018-07-20 11:20:30 +0100

    feat: Add ARIA roles to elements

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -187,6 +187,7 @@ createReviewHTML = (review) => {
 fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
+  li.setAttribute("aria-current", "page");
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
 }
